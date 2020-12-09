be84776e249bfd8d7829f560656be2c225e81f30
Syed Souban <syedsouban890@gmail.com>
2020-12-10 03:07:37 +0530

    updated session id expiry to 7 days

--- models/session.py
+++ models/session.py
@@ -17,7 +17,7 @@ class UserSessions(db.DynamicDocument):
         user_session = {
             fields.user_id: user_id,
             fields.session_id: session_id,
-            fields.expiry_date: get_time_after(hours=12, minutes=0),
+            fields.expiry_date: get_time_after(days = 30, hours=0, minutes=0),
             fields.email:email,    
             fields.is_active:True
         }
