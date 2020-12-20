4c758bb7d2e10cdbe225707de23aca5386b8fe64
Syed Souban <syedsouban890@gmail.com>
2020-12-20 22:00:57 +0530

    missed removing .main dep

--- routes/chat.py
+++ routes/chat.py
@@ -1,6 +1,5 @@
 from app import app
 from flask import session, redirect, url_for, render_template, request
-from . import main
 from forms.forms import LoginForm
 
 @app.route('/', methods=['GET', 'POST'])
