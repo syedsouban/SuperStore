9553263ed9c0589c27266eabaacb8f8d289666c3
Syed Souban <syedsouban890@gmail.com>
2020-12-28 21:13:49 +0530

    merged dev into master

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 date=$(date '+%Y-%m-%d')
 if [ ! -d ./logs/ ] 
 then
@@ -6,7 +5,3 @@ then
 fi
 
 sudo ./SuperStore/bin/gunicorn app:app -w 2 --bind=0.0.0.0:80 --access-logfile ./logs/$date.txt --log-level=debug &
-=======
-mkdir logs
-sudo ./SuperStore/bin/gunicorn --worker-class eventlet -w 1 app:app --bind=0.0.0.0:8080 --access-logfile logs/devel_logs.txt --log-level=debug &
->>>>>>> dev
