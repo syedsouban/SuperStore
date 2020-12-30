8c788b0599e3d02df4871d9e1db70c4d5ec35db2
Syed Souban <syedsouban890@gmail.com>
2020-12-30 20:08:59 +0530

    used socket_io namespace/endpoint everywhere

--- events/events.py
+++ events/events.py
@@ -3,7 +3,7 @@ from flask_socketio import emit, join_room, leave_room
 from app import socketio
 
 
-@socketio.on('joined', namespace='/chat')
+@socketio.on('joined', namespace='/socket_io')
 def joined(message):
     print("joined")
     """Sent by clients when they enter a room.
@@ -13,7 +13,7 @@ def joined(message):
     emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
 
 
-@socketio.on('text', namespace='/chat')
+@socketio.on('text', namespace='/socket_io')
 def text(message):
     """Sent by a client when the user entered a new message.
     The message is sent to all people in the room."""
@@ -21,7 +21,7 @@ def text(message):
     emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
 
 
-@socketio.on('left', namespace='/chat')
+@socketio.on('left', namespace='/socket_io')
 def left(message):
     """Sent by clients when they leave a room.
     A status message is broadcast to all people in the room."""
