b8b09aacadf87d46ceeee3e5336831820907299b
Syed Souban <syedsouban890@gmail.com>
2021-01-17 12:32:08 +0530

    missed aws utils in last commit

---
+++ utils/aws.py
@@ -0,0 +1,48 @@
+from app import photos
+from dotenv import load_dotenv
+import os
+import boto3
+
+load_dotenv(verbose=True)
+
+
+def aws_session(region_name='us-east-1'):
+    return boto3.session.Session(aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                                aws_secret_access_key=os.getenv('AWS_ACCESS_KEY_SECRET'),
+                                region_name=region_name)
+
+
+def make_bucket(name, acl):
+    session = aws_session()
+    s3_resource = session.resource('s3')
+    return s3_resource.create_bucket(Bucket=name, ACL=acl)
+
+
+def upload_file_to_bucket(bucket_name, file_path):
+    session = aws_session()
+    s3_resource = session.resource('s3')
+    file_dir, file_name = os.path.split(file_path)
+
+    bucket = s3_resource.Bucket(bucket_name)
+    bucket.upload_file(
+      Filename=file_path,
+      Key=file_name,
+      ExtraArgs={'ACL': 'public-read'}
+    )
+
+    s3_url = f"https://{bucket_name}.s3.amazonaws.com/{file_name}"
+    return s3_url
+
+
+def download_file_from_bucket(bucket_name, s3_key, dst_path):
+    session = aws_session()
+    s3_resource = session.resource('s3')
+    bucket = s3_resource.Bucket(bucket_name)
+    bucket.download_file(Key=s3_key, Filename=dst_path)
+
+def upload_image(request):
+    file = request.files['photo']
+    filename = photos.save(file)
+    # s3_bucket = make_bucket(os.getenv('S3_IMAGES_BUCKET'), 'public-read')
+    image_url = upload_file_to_bucket(os.getenv('S3_IMAGES_BUCKET'), filename)
+    return image_url
